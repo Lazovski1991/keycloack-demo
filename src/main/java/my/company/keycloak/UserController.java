bc7e757ec3c6982044b3066ddede62c97b485f8f
alazovskiy <alexandr.lazovskiy@shtormtech.ru>
2022-04-16 17:11:49 +0300

    init

---
+++ src/main/java/my/company/keycloak/UserController.java
@@ -0,0 +1,15 @@
+package my.company.keycloak;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/current")
+    public String getCurrentUser() {
+        return "YAYAYAYAYA";
+    }
+}
